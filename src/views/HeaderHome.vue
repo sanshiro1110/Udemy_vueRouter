fff9e4cd40ac243402969ca99a24b2e92b431f34
sanshiro1110 <66013439+sanshiro1110@users.noreply.github.com>
2020-10-05 23:01:25 +0900

    Add files via upload

---
+++ src/views/HeaderHome.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <h2>Home</h2>
+    <router-link
+      to="/"
+      class="link"
+      active-class="link--active"
+      exact
+    >Home</router-link>
+    <router-link
+      :to="{name: 'users-id-profile', params: { id: 1}, query: {lang: 'ja', page: 2}, hash: '#next-user'}"
+      class="link"
+      active-class="link--active"
+      exact
+    >Users</router-link>
+    </nav>
+</template>
+
+<style scoped>
+  .link {
+    margin-right: 10px;
+  }
+
+  .link--active {
+    font-size: 20px;
+  }
+</style>
